d56c1a19445bd9fe8a984dadacb566e7707ff8b1
MaximeMonterin <maxime.monterin@etu.univ-amu.fr>
2022-11-29 20:50:36 +0100

    ajout des images des cases pour le plateau

--- src/main/java/com/mmn/dames/Dames.java
+++ src/main/java/com/mmn/dames/Dames.java
@@ -3,6 +3,11 @@ package com.mmn.dames;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +15,15 @@ import java.io.IOException;
 public class Dames extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Dames.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 2000, 1000);
+
+        Pane pane = new HBox();
+
+        Image brownCase = new Image("C:\\Users\\under\\IdeaProjects\\Dames\\images\\brown_case.png");
+        Image skinCase = new Image("C:\\Users\\under\\IdeaProjects\\Dames\\images\\skin_case.png");
+        pane.getChildren().add(new ImageView(brownCase)); pane.getChildren().add(new ImageView(skinCase));
+
+        Scene scene = new Scene(pane, 2000, 1000);
+        scene.setFill(Color.web("1d3557"));
         stage.setTitle("Dames");
         stage.setScene(scene);
         stage.show();
