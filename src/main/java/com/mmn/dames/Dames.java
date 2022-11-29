899c3bfddfd6433ca8692e5274f431b9b70f353a
MaximeMonterin <maxime.monterin@etu.univ-amu.fr>
2022-11-29 21:30:01 +0100

    Création graphique du plateau de jeu

--- src/main/java/com/mmn/dames/Dames.java
+++ src/main/java/com/mmn/dames/Dames.java
@@ -3,11 +3,8 @@ package com.mmn.dames;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,17 +13,15 @@ public class Dames extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Pane pane = new HBox();
+        Pane pane = new VBox();
+        Plateau plateau = new Plateau();
 
-        Image brownCase = new Image("C:\\Users\\under\\IdeaProjects\\Dames\\images\\brown_case.png");
-        Image skinCase = new Image("C:\\Users\\under\\IdeaProjects\\Dames\\images\\skin_case.png");
-        pane.getChildren().add(new ImageView(brownCase)); pane.getChildren().add(new ImageView(skinCase));
-
-        Scene scene = new Scene(pane, 2000, 1000);
-        scene.setFill(Color.web("1d3557"));
+        Scene scene = new Scene(plateau.loadPlateau(pane), 1000, 1000);
         stage.setTitle("Dames");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/mmn/dames/Plateau.java
+++ src/main/java/com/mmn/dames/Plateau.java
@@ -1,5 +1,12 @@
 package com.mmn.dames;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+import java.io.*;
+
 public class Plateau {
     //ATTRIBUTS
     private final int NB_CASES_NOIRES = 50;
@@ -8,6 +15,29 @@ public class Plateau {
 
     //CONSTRUCTOR
     public Plateau() {}
+    public Pane loadPlateau(Pane pane) throws IOException {
+
+        File filePlateau = new File("C:\\Users\\under\\IdeaProjects\\Dames\\plateau.txt");
+        Image brownCase = new Image("C:\\Users\\under\\IdeaProjects\\Dames\\images\\brown_case.png");
+        Image skinCase = new Image("C:\\Users\\under\\IdeaProjects\\Dames\\images\\skin_case.png");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePlateau), "UTF-8"));
+        String line = reader.readLine();
+
+        while(line != null){
+            HBox linePlateau = new HBox();
+            for(int i = 0 ; i < line.length() ; ++i){
+                if(Character.getNumericValue(line.charAt(i)) == 0){linePlateau.getChildren().add(new ImageView(brownCase));}
+                else{linePlateau.getChildren().add(new ImageView(skinCase));}
+            }
+            pane.getChildren().add(linePlateau);
+            line = reader.readLine();
+        }
+
+        reader.close();
+
+        return pane;
+    }
 
     //GETTERS
     public int getNB_CASES_NOIRES() {
