4082a515f57f2814be36b55cdae2c0e72337138d
Maxime Monterin <maxime.monterin@etu.univ-amu.fr>
2022-11-30 11:47:22 +0100

    refractor de la creation du plateau

--- src/main/java/com/mmn/dames/Plateau.java
+++ src/main/java/com/mmn/dames/Plateau.java
@@ -46,12 +46,6 @@ public class Plateau {
                 }
             }
         }
-
-        for(int i = 0; i < 10 ; ++i){
-            for(int j = 0; j < 10 ; ++j){
-                System.out.println(matricePlateau[i][j].toString());
-            }
-        }
     }
 
     //GETTERS
